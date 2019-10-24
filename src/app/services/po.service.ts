5b244515c44eafe9147dd25c5dd0cf809637f8e4
Elisheva Nachaysi <elishnch@gmail.com>
2019-10-24 07:01:42 +0300

    fixed bugs

--- src/app/services/po.service.ts
+++ src/app/services/po.service.ts
@@ -14,7 +14,7 @@ export class PoService {
 
   public getImage(itemCode:string)
   {
-    return this.http.get('http://localhost:54530/api/po/ImageSRC/' + itemCode);
+    return this.http.get('https://www.elitedistribution.soccer/EliteAPI/api/po/ImageSRC/' + itemCode);
 
   }
 
@@ -22,14 +22,14 @@ export class PoService {
   {
     
   
-   return this.http.get('http://localhost:54530/api/po/main/' +  OD1 +'/' + OD2+ '/'+ localStorage.getItem("currentUser")) ;
+   return this.http.get('https://www.elitedistribution.soccer/EliteAPI/api/po/main/' +  OD1 +'/' + OD2+ '/'+ localStorage.getItem("currentUser")) ;
    
   }
 
 public getPoDetails(id)
 {
  let po:Po;
- return  this.http.get('http://localhost:54530/api/po/Details/' + id);
+ return  this.http.get('https://www.elitedistribution.soccer/EliteAPI/api/po/Details/' + id);
   // .subscribe(
   //   data=>{
   //    return  (data as Po);
@@ -43,7 +43,7 @@ public getPoDetails(id)
 public updatePO(po:Po) 
 {
 
-   return this.http.put <Response> ('http://localhost:54530/api/po'  ,po,{observe: 'response'});
+   return this.http.put <Response> ('https://www.elitedistribution.soccer/EliteAPI/api/po'  ,po,{observe: 'response'});
    // return this.res 
     
 
@@ -52,12 +52,12 @@ public updatePO(po:Po)
 
 public addPo(po:Po)
 {
-  return this.http.post <Response> ('http://localhost:54530/api/po'  ,po,{observe: 'response'});
+  return this.http.post <Response> ('https://www.elitedistribution.soccer/EliteAPI/api/po'  ,po,{observe: 'response'});
 
 }
 
 public getItemsToNewPo()
 {
-  return this.http.get('http://localhost:54530/api/po/Details');
+  return this.http.get('https://www.elitedistribution.soccer/EliteAPI/api/po/Details');
 }
 }
